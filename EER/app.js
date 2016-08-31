50c8644364bf7d86ed6c4252f4b8565758e55e73
Shripad <shripad.joshi.s@gmail.com.com>
2016-08-31 16:20:07 +0530

    datatable and company changes

--- EER/app.js
+++ EER/app.js
@@ -3,10 +3,23 @@
 // Declare app level module which depends on views, and components
 angular.module('EERApp', [
     'ngRoute',
+    'datatables',
     'EERApp.home',
     'EERApp.company'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+
+.filter('capitalize', function() {
+
+    // In the return function, we must pass in a single parameter which will be the data we will work on.
+    // We have the ability to support multiple other parameters that can be passed into the filter optionally
+    return function(input, optional1, optional2) {
+        //return JSON.stringify(JSON.parse(input), null, 2);
+        var firstChar = input.charAt(0).toUpperCase();
+        return (input.replace(input.charAt(0), firstChar))
+    }
+
+})
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'home/home.html',
--- EER/company/company.js
+++ EER/company/company.js
@@ -2,6 +2,26 @@
 
 angular.module('EERApp.company', ['ngRoute'])
 
+.factory('CompanyDetails', ['$http', function($http) {
+    return {
+        get: function() {
+            return $http.get('http://localhost:1337/company');
+        },
+        /*getBrowser: function(browserId) {
+            return $http.get('http://localhost:1337/browser/' + browserId);
+        },
+        updateBrowser: function(browserId, name) {
+            return $http.put('http://localhost:1337/browser/' + browserId, { name: name })
+        },
+        deleteBrowser: function(browserId) {
+            return $http.delete('http://localhost:1337/browser/' + browserId)
+        },
+        createNewBrowser: function(name) {
+            return $http.post('http://localhost:1337/browser', { name: name })
+        }*/
+    }
+}])
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/companies', {
     templateUrl: 'company/companies.html',
@@ -9,6 +29,28 @@ angular.module('EERApp.company', ['ngRoute'])
   });
 }])
 
-.controller('CompanyController', ['$rootScope', '$scope', '$routeParams',function($rootScope, $scope, $routeParams) {
+.controller('CompanyController', ['$rootScope', '$scope', '$routeParams', 'CompanyDetails', '$filter',
+	function($rootScope, $scope, $routeParams, CompanyDetails, $filter) {
 	$rootScope.activeTab = "Company";
+	$scope.companies = "";
+    $scope.message = "";
+    $scope.errMessage = "";
+    $scope.isMsg = false;
+    $scope.isErr = false;
+
+    //This will fetch all the available browsers
+    CompanyDetails.get()
+        .success(function(data) {
+            $scope.companies = data;
+            /*var allCount = [];
+            angular.forEach($scope.browsers, function(obj) {
+                allCount.push(obj.name);
+            });
+            $scope.allBrowsers = allCount;*/
+        }).error(function(err) {
+            $scope.message = "";
+            $scope.isMsg = false;
+            $scope.errMessage = err.message;
+            $scope.isErr = true;
+        });
 }]);
