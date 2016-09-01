4da50e6f79b4e90129932e5b25952b30d5d56682
Shripad <shripad.joshi.s@gmail.com.com>
2016-09-01 17:33:26 +0530

    create company changes

--- EER/company/company.js
+++ EER/company/company.js
@@ -26,31 +26,133 @@ angular.module('EERApp.company', ['ngRoute'])
   $routeProvider.when('/companies', {
     templateUrl: 'company/companies.html',
     controller: 'CompanyController'
+  }).when('/company/new',{
+  	templateUrl: 'company/new.html',
+    controller: 'CompanyController'
   });
 }])
 
 .controller('CompanyController', ['$rootScope', '$scope', '$routeParams', 'CompanyDetails', '$filter',
 	function($rootScope, $scope, $routeParams, CompanyDetails, $filter) {
+	//This rootScope variable will set the active tab in the menu item
 	$rootScope.activeTab = "Company";
+
 	$scope.companies = "";
     $scope.message = "";
     $scope.errMessage = "";
     $scope.isMsg = false;
     $scope.isErr = false;
+    $scope.isCompanyExist = false;
+    $scope.isCompanyWebsiteExist = false;
+    $scope.isValidWebsite = false;
+    $scope.companyForm = {
+        loading: false
+    };
+    $scope.allCompanies = "";
+    $scope.allWebsites = "";
+    $scope.urlRegex = new RegExp(
+            "^" +
+            // protocol identifier
+            "(?:(?:https?|ftp)://)" +
+            // user:pass authentication
+            "(?:\\S+(?::\\S*)?@)?" +
+            "(?:" +
+            // IP address exclusion
+            // private & local networks
+            "(?!10(?:\\.\\d{1,3}){3})" +
+            "(?!127(?:\\.\\d{1,3}){3})" +
+            "(?!169\\.254(?:\\.\\d{1,3}){2})" +
+            "(?!192\\.168(?:\\.\\d{1,3}){2})" +
+            "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+            // IP address dotted notation octets
+            // excludes loopback network 0.0.0.0
+            // excludes reserved space >= 224.0.0.0
+            // excludes network & broacast addresses
+            // (first & last IP address of each class)
+            "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+            "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+            "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+            "|" +
+            // IPv6 RegEx - http://stackoverflow.com/a/17871737/273668
+            "\\[(" +
+            "([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|" + // 1:2:3:4:5:6:7:8
+            "([0-9a-fA-F]{1,4}:){1,7}:|" + // 1::                              1:2:3:4:5:6:7::
+            "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|" + // 1::8             1:2:3:4:5:6::8  1:2:3:4:5:6::8
+            "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" + // 1::7:8           1:2:3:4:5::7:8  1:2:3:4:5::8
+            "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|" + // 1::6:7:8         1:2:3:4::6:7:8  1:2:3:4::8
+            "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|" + // 1::5:6:7:8       1:2:3::5:6:7:8  1:2:3::8
+            "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|" + // 1::4:5:6:7:8     1:2::4:5:6:7:8  1:2::8
+            "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|" + // 1::3:4:5:6:7:8   1::3:4:5:6:7:8  1::8  
+            ":((:[0-9a-fA-F]{1,4}){1,7}|:)|" + // ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8 ::8       ::  
+            "fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|" + // fe80::7:8%eth0   fe80::7:8%1     (link-local IPv6 addresses with zone index)
+            "::(ffff(:0{1,4}){0,1}:){0,1}" +
+            "((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}" +
+            "(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|" + // ::255.255.255.255   ::ffff:255.255.255.255  ::ffff:0:255.255.255.255  (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
+            "([0-9a-fA-F]{1,4}:){1,4}:" +
+            "((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}" +
+            "(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])" + // 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+            ")\\]" +
+            "|" +
+            "localhost" +
+            "|" +
+            // host name
+            "(?:xn--[a-z0-9\\-]{1,59}|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))" +
+            // domain name
+            "(?:\\.(?:xn--[a-z0-9\\-]{1,59}|(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))*" +
+            // TLD identifier
+            "(?:\\.(?:xn--[a-z0-9\\-]{1,59}|(?:[a-z\\u00a1-\\uffff]{2,63})))" +
+            ")" +
+            // port number
+            "(?::\\d{2,5})?" +
+            // resource path
+            "(?:/[^\\s]*)?" +
+            "$", "i"
+        );
 
-    //This will fetch all the available browsers
+
+    //This will fetch all the available companies
     CompanyDetails.get()
         .success(function(data) {
             $scope.companies = data;
-            /*var allCount = [];
-            angular.forEach($scope.browsers, function(obj) {
-                allCount.push(obj.name);
+            var allComps = [];
+            var allWebs = [];
+            angular.forEach($scope.companies, function(obj) {
+                allComps.push(obj.name);
+                allWebs.push(obj.website);
             });
-            $scope.allBrowsers = allCount;*/
+            $scope.allCompanies = allComps;
+            $scope.allWebsites = allWebs;
         }).error(function(err) {
             $scope.message = "";
             $scope.isMsg = false;
             $scope.errMessage = err.message;
             $scope.isErr = true;
         });
+
+    $scope.validateCompanyExist = function(companyName){
+		if ($scope.allCompanies.indexOf(companyName) > -1) {
+	        $scope.isCompanyExist = true;
+	    } else {
+	        $scope.isCompanyExist = false;
+	    }
+    },
+
+    $scope.validateCompanyWebsiteExist = function(companyWebsite){
+		if ($scope.allWebsites.indexOf(companyWebsite) > -1) {
+	        $scope.isCompanyWebsiteExist = true;
+	    } else {
+	        $scope.isCompanyWebsiteExist = false;
+	    }
+    },
+
+    $scope.validateCompanyWebsite = function(companyWebsite){
+		if($scope.urlRegex.test(companyWebsite)) {
+            $scope.isValidWebsite = true;
+        } else {
+            $scope.isValidWebsite = false;
+        }
+    }
+
+    
+
 }]);
