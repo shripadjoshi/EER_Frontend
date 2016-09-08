df66dcad6d000668d07f6928d2835b408e511c78
Shripad <shripad.joshi.s@gmail.com.com>
2016-09-08 12:05:27 +0530

    company UI changes

--- EER/app.js
+++ EER/app.js
@@ -27,6 +27,18 @@ angular.module('EERApp', [
     }
 })
 
+.filter('joinArrayValues', function() {
+    // In the return function, we must pass in a single parameter which will be the data we will work on.
+    // We have the ability to support multiple other parameters that can be passed into the filter optionally
+    return function(input, optional1, optional2) {
+        var values = []
+        angular.forEach(input, function(obj) {
+            values.push(obj.name);
+        });
+        return (values.join(", "));
+    }
+})
+
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/', {
